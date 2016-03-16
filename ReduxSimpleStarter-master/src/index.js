55fcb4b3a473e6e0f159b141297ba696c3488e9c
Mike <mvg210@gmail.com>
2016-03-16 13:39:12 -0700

    Tested youtube API

--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import SearchBar from './components/search_bar'
+import YTSearch from 'youtube-api-search'
 
 const API_KEY = 'AIzaSyByN7djh_vobbprHVnb8XXZB-h_14DfjLs'
-// Create a new component
-// Should produce some HTML
+YTSearch({key: API_KEY}, function(results){
+  console.log(results);
+})
 const App = () => {
   return (
     <div><SearchBar /></div>
   )
 }
-
-// Take this component's generated HTML and put it on the DOM
 ReactDOM.render(<App />, document.querySelector('.container'))
