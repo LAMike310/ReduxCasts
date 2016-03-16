4a2a18c960cdb5555c8e45a6bb7472bff20695dd
Mike <mvg210@gmail.com>
2016-03-16 11:50:32 -0700

    Created components folder

---
+++ ReduxSimpleStarter-master/src/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Component } from 'react';
+
+const API_KEY = 'AIzaSyByN7djh_vobbprHVnb8XXZB-h_14DfjLs'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>Yoooo</div>
+    );
+  }
+}
