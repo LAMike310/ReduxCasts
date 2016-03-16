6e98e1abdd70ce2f0eeb99eb5eb4006bf5ad0eb9
Mike <mvg210@gmail.com>
2016-03-16 14:17:57 -0700

    Added video list component

--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 import SearchBar from './components/search_bar'
 import YTSearch from 'youtube-api-search'
-
+import VideoList from './components/video_list'
 const API_KEY = 'AIzaSyByN7djh_vobbprHVnb8XXZB-h_14DfjLs'
-YTSearch({key: API_KEY}, function(results){
-  console.log(results);
-})
-const App = () => {
-  return (
-    <div><SearchBar /></div>
-  )
+class App extends Component {
+  constructor(props){
+    super(props)
+    YTSearch({key: API_KEY}, (videos) => {
+      console.log(videos);
+      this.setState({ videos })
+      // this.setState({ videos: videos })
+    })
+    this.state = { videos: [] }
+  }
+  render(){
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    )
+  }
 }
 ReactDOM.render(<App />, document.querySelector('.container'))
